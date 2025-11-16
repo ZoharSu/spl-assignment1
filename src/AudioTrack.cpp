2cc0472b696a05ee5fc65962ebf09c242fb9f8e4
zoxarc <zoxarc@gmail.com>
2025-11-16 21:47:16 +0200

    AudioTrack: Copy Constructor and Assignment

--- src/AudioTrack.cpp
+++ src/AudioTrack.cpp
@@ -36,15 +36,21 @@ AudioTrack::~AudioTrack() {
     std::cout << "AudioTrack destructor called for: " << title << std::endl;
     #endif
     // Your code here...
+    delete waveform_data;
 }
 
 AudioTrack::AudioTrack(const AudioTrack& other)
+    : title(other.title), artists(other.artists), duration_seconds(other.duration_seconds),
+      bpm(other.bpm), waveform_size(other.waveform_size)
 {
     // TODO: Implement the copy constructor
     #ifdef DEBUG
     std::cout << "AudioTrack copy constructor called for: " << other.title << std::endl;
     #endif
     // Your code here...
+    waveform_data = new double[waveform_size];
+    for (int i = 0; i < waveform_size; i++)
+        waveform_data[i] = other.waveform_data[i];
 }
 
 AudioTrack& AudioTrack::operator=(const AudioTrack& other) {
@@ -53,6 +59,18 @@ AudioTrack& AudioTrack::operator=(const AudioTrack& other) {
     std::cout << "AudioTrack copy assignment called for: " << other.title << std::endl;
     #endif
     // Your code here...
+    delete waveform_data;
+
+    title = other.title;
+    artists = other.artists;
+    duration_seconds = other.duration_seconds;
+    bpm = other.bpm;
+    waveform_size = other.waveform_size
+
+    waveform_data = new double[waveform_size];
+    for (int i = 0; i < waveform_size; i++)
+        waveform_data[i] = other.waveform_data[i];
+
     return *this;
 }
 
