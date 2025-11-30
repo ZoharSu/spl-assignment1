c876366c9a38e73558ca638b470f7539e1a07af9
zoxarc <zoxarc@gmail.com>
2025-11-30 13:35:01 +0200

    Finish `MixingEngineService`, add `AudioTrack::set_bpm`

--- src/MixingEngineService.cpp
+++ src/MixingEngineService.cpp
@@ -90,6 +90,9 @@ void MixingEngineService::displayDeckStatus() const {
  * @return: true if BPM difference <= tolerance, false otherwise
  */
 bool MixingEngineService::can_mix_tracks(const PointerWrapper<AudioTrack>& track) const {
+    if (decks[active_deck] == nullptr || track)
+        return false;
+
     int bpm_diff = track->get_bpm() - decks[active_deck]->get_bpm();
     if (bpm_diff < 0) bpm_diff *= -1; // apply absolute value
 
@@ -101,5 +104,10 @@ bool MixingEngineService::can_mix_tracks(const PointerWrapper<AudioTrack>& track
  * @param track: Track to synchronize with active deck
  */
 void MixingEngineService::sync_bpm(const PointerWrapper<AudioTrack>& track) const {
-    // Your implementation here
+    if (decks[active_deck] == nullptr || track)
+        return;
+
+    int bpm_avg = (decks[active_deck]->get_bpm() + track->get_bpm()) / 2;
+
+    track->set_bpm(bpm_avg);
 }
